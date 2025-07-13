d74d210e3d627fc36989f9b5816e73be27ca78a5
Kapil <kapilmcdave@gmail.com>
2025-07-13 13:39:27 -0700

    day5update

--- day5.py
+++ day5.py
@@ -1,1 +1,20 @@
-print("Welcome to Day5")
+print("Movie Character Guesser")
+print()
+
+spiderman = input("Do you like hanging around?")
+if spiderman == "yes":
+    print("hello spiderman!")
+else:
+    print("Me neither")
+
+batman = input("Can you take a bite out of crime?")
+if batman == "yes":
+    print("Summon the batmobile!")
+else:
+    print("Oh well...")
+
+strange = input("can you do magic?")
+if strange == "yes":
+    print("nice")
+else:
+    print("I guess you aren't super after all :()")
