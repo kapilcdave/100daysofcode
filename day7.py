645fb6a98bd38437d98207d57d9c9792221c06a3
Kapil <kapilmcdave@gmail.com>
2025-07-15 14:57:01 -0700

    updated day 7 len function

--- day7.py
+++ day7.py
@@ -1,3 +1,5 @@
+print("TV Show Check")
+print("-------------")
 tvshow = input("what is your favorite tv show?")
 if tvshow == "peppa pig":
     print("why?")
@@ -9,4 +11,7 @@ if tvshow == "peppa pig":
 elif tvshow == ("Thomas the Tank Engine"):
     print("good choice")
 else:
-    print("please name an actual tv show")
+    print("I've never heard of that show")
+    tvshowlength = input("can you spell the tv show's name?")
+    if len(tvshowlength) <= 11:
+        print("If you can't spell it then it doesn't exist")
