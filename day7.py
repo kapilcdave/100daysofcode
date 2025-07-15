4b913b9974f531bca88913bdf2e939d7ac241b91
Kapil <kapilmcdave@gmail.com>
2025-07-15 14:50:06 -0700

    day7 nesting

---
+++ day7.py
@@ -0,0 +1,12 @@
+tvshow = input("what is your favorite tv show?")
+if tvshow == "peppa pig":
+    print("why?")
+    favcharacter = input("who is your favorite character?")
+    if favcharacter == "daddy pig":
+        print("correct!")
+    else:
+        print("thanks for sharing")
+elif tvshow == ("Thomas the Tank Engine"):
+    print("good choice")
+else:
+    print("please name an actual tv show")
