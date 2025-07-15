49ee5a7804e6d5c9b8ec2afb04415cffc70f68c6
Kapil <kapilmcdave@gmail.com>
2025-07-14 22:38:43 -0700

    day6 completed challenge

---
+++ day6.py
@@ -0,0 +1,10 @@
+print("Secure Login")
+username = input("Username >")
+if username =="Mark":
+    print("Welcome Mark, I'm happy to meet you")
+elif username == "Suzanne":
+    print("Welcome Suzanne, I'm sorry to meet you")
+elif username == "Orion":
+    print("Sign-in again")
+else:
+    print("Unauthorized Login Attempt")
