96bd6f3e506fbfc0a693fd9e6451282230e13be1
Kapil <kapilmcdave@gmail.com>
2025-07-20 11:09:28 -0700

    day13 grade

---
+++ day13.py
@@ -0,0 +1,18 @@
+print("Exam Grade Calculator")
+print()
+grade = int(input("Enter your grade (0-100): "))
+if grade >= 90:
+    print("You got an A!")
+elif grade >= 80:
+    print("You got a B!")
+elif grade >= 70:
+    print("You got a C!")
+elif grade >= 60:
+    print("You got a D!")
+elif grade >= 50:
+    print("You got an E!")
+else:
+    print("You got an F!")
+print("Thank you for using the Exam Grade Calculator!")
+print("Goodbye!")
+print()
