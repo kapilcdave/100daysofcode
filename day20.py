f72eaf77cd59831b2716a1a3872015a89b41efde
Kapil <kapilmcdave@gmail.com>
2025-08-06 16:34:17 -0700

    list generate

---
+++ day20.py
@@ -0,0 +1,8 @@
+print("List Generator")
+print()
+start = input("Start at:")
+end = input("End at:")
+increment = input("Increment:")
+for i in range(int(start), int(end), int(increment)):
+    print(i)
+exit()
