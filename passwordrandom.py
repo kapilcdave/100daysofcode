3355c28c6eb1d62e2cce224e1b98c45d156dd730
kapilcdave <kapilmcdave@gmail.com>
2025-12-25 00:53:15 -0800

    dumb password

--- passwordrandom.py
+++ passwordrandom.py
@@ -6,3 +6,15 @@ all_chars = string.ascii_letters + string.digits + string.punctuation
 
 password = ''.join(random.choices(all_chars, k=19))
 print(password)
+
+while True:
+    user_input = input("Enter the password: ")
+    if user_input == password:
+        print("Password is correct")
+        break
+    if user_input == "help":
+        print("Password is", password)
+        continue
+    else:
+        print("Password is incorrect")
+        continue
